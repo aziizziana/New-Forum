6c94e116b48d7531f338647abf9875e88b3fb33b
Aziiz Ziana <aziizziana@gmail.com>
2013-10-29 14:24:54 +0300

    Forum Code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,4 +16,15 @@ class QuestionsController < ApplicationController
 		redirect_to questions_path
 	end
 
+	def edit
+		@questions = Question.find(params[:id])
+	end
+
+	def update
+		@questions = Question.find(params[:id])
+		@questions.update_attributes(params[:questions])
+		render :text => "Updated successfully!!"
+		
+	end
+
 end
